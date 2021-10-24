5c11d3623edd3ee2d367381aa4b0705977ff2de9
wissemayadi <wissem-ayadi@hotmail.com>
2021-10-24 19:30:59 +0100

    crud done

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,9 +1,27 @@
+const Transaction = require('../models/Transaction');
+
+
 //@desc Get all transaction
 //@route GET /api/v1/transactions
 //@access Public
-exports.getTransactions= (req,res,next)=>{
+exports.getTransactions= async (req,res,next)=>{
 
-res.send('GET transactions');
+try {
+    const transactions = await Transaction.find();
+  return res.status(200).json({
+  success :true,
+  count: transactions.length,
+  data: transactions
+
+  })
+
+} catch (err) {
+    return res.status(500).json({
+   success:false,
+   error: 'Server Error'
+
+    })
+}
 
 
 }
@@ -11,19 +29,63 @@ res.send('GET transactions');
 //@desc Add transaction
 //@route POST /api/v1/transactions
 //@access Public
-exports.addTransactions= (req,res,next)=>{
-
-    res.send('POST transactions');
+exports.addTransactions= async(req,res,next)=>{
+    try {
+        const {text,amout} = req.body;
+        const transaction = await Transaction.create(req.body);
     
+        return res.status(201).json({
+     success:true,
+     data:transaction
+    
+        })
+    } catch (err) {
+        if(err.name==='ValidationError'){
+     const messages =Object.values(err.errors).map(val => val.message)
+     return res.status(400).json({
+         success:false,
+         error:messages
+     })
+        }else{
+            return res.status(500).json({
+                success:false,
+                error:'Server Error'
+               
+                   })
+        }
+    }
+   
     
     }
 
 //@desc DELETE transaction
 //@route DELETE /api/v1/transactions
 //@access Public
-exports.deleteTransaction= (req,res,next)=>{
+exports.deleteTransaction=async (req,res,next)=>{
 
-    res.send('DELETE transactions');
+  try {
+
+    const transaction = await Transaction.findById(req.params.id); 
+   
+           if(!transaction){
+            return res.status(201).json({
+                success:false,
+                error:'No transaction'
+               
+                   })
+           }
+
+  await transaction.remove();
+  return res.status(201).json({
+    success:true,
+    data:{}
+   
+       })
+  } catch (err) {
+      console.log(err)
+  }
     
     
     }
+
+    
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express =require('express');
 const dotenv=require('dotenv');
 const colors=require('colors');
 const morgan =require('morgan');
+const connectDB = require('./config/db')
 const transactions= require('./route/transactions');
 dotenv.config({path:'./config/config.env'});
+connectDB();
 const app=express();
+
+app.use(express.json());
 
 app.use('/api/v1/transactions',transactions)
 // transactions.get('/',(req,res)=>{
