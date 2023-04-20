6dfdf51754e3141918c7b3546137152d4279f9c2
wissemayadi <wissem-ayadi@hotmail.com>
2023-04-20 02:37:49 +0100

    install cors

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const path=require('path');
 const express =require('express');
 const dotenv=require('dotenv');
 const colors=require('colors');
+const cors =require('cors');
 const morgan =require('morgan');
 const connectDB = require('./config/db')
 const transactions= require('./route/transactions');
 dotenv.config({path:'./config/config.env'});
 connectDB();
 const app=express();
-
+app.use(cors);
 app.use(express.json());
 if(process.env.NODE_ENV === 'development'){
 app.use(morgan('dev'));
